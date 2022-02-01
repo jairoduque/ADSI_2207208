34e6c92d3dcf9428fac91cb5ce218d9c5e8772e9
jairo alonso duque arias <jairoa51947@hotmail.com>
2022-01-31 19:15:11 -0500

    creando operadores matematicos

---
+++ operadoresArit.php
@@ -0,0 +1,18 @@
+<?php
+$num1=5;
+$num2=4;
+$suma=$num1+$num2;
+$res=$num1-$num2;
+$mult=$num1*$num2;
+$div=$num1/$num2;
+echo "la suma es: " .$suma;
+echo "<br>";
+echo "la resta es :" .$res;
+echo "<br>";
+echo "la multiplicacion es: " .$mult;
+echo "<br>";
+echo "la división es: " .$div;
+echo "<br>";
+
+
+
--- variables.php
+++ variables.php
@@ -1,3 +1,5 @@
 <?php
 define("usuario","jairo");
 echo constant('usuario');
+//no le pongo el signo $ ya que se encientra definido com ouna constante
+echo usuario;
